d8f84321de7521a85aaa9e256c9b8b1f63486b88
irina glazunova <zvepushka0@gmail.com>
2022-01-13 22:11:04 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Irina");
+        System.out.println("30.09.1993");
+    }
+}
