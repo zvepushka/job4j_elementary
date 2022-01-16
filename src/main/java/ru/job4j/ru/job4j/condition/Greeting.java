437f8c6857a428f68882af70bc99d95bec4e68f3
irina glazunova <zvepushka0@gmail.com>
2022-01-16 21:15:56 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+    String idea = "I love Java!";
+    String addition = "But I am a newbie.";
+    String separate = "  ";
+    int year = 2022;
+    System.out.println(idea + separate + addition + separate + year);
+    }
+}
