eb5520b26f539f0014ef1f59c50f7364dcc2e51b
irina glazunova <zvepushka0@gmail.com>
2022-01-18 21:59:19 +0300

    1. Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.ru.job4j.converter;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void rubleToEuro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void rubleToDollar() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
