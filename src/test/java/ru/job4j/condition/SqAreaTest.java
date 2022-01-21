76be5ad21321c60ce6bdf0031840f11517727704
irina glazunova <zvepushka0@gmail.com>
2022-01-21 09:22:25 +0300

    4. Тесты для стороны прямоугольника. [#159460 #253577]

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -26,10 +26,10 @@ public class SqAreaTest {
 
     @Test
 
-    public void whenP28K7Square28() {
-        int expected = 2;
-        int p = 6;
-        double k = 2;
+    public void whenP28K6Square24() {
+        int expected = 24;
+        int p = 28;
+        double k = 6;
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
     }
