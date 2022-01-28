02f43f3860dc536d7721ed70f4fb648710d647c3
irina glazunova <zvepushka0@gmail.com>
2022-01-28 20:15:06 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,9 +9,23 @@ public class Counter {
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 4));
-        System.out.println(sum(3, 5));
-        System.out.println(sum(1, 1));
-    }
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int index = start; index <= finish; index++) {
+            if (index % 2 == 0) {
+                sum = sum + index;
+            }
+        }
+            return sum;
+        }
+
+        public static void main(String[] args) {
+            System.out.println(sum(0, 10));
+            System.out.println(sum(3, 8));
+            System.out.println(sum(1, 1));
+
+            System.out.println(sumByEven(0, 10));
+            System.out.println(sumByEven(3, 8));
+            System.out.println(sumByEven(1, 1));
+        }
 }
---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        Assert.assertEquals(expected, result);
+    }
+}
