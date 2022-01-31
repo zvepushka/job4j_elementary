53722a931a4cb000d2ddaebb8c1449d58f463f31
irina glazunova <zvepushka0@gmail.com>
2022-01-31 13:34:54 +0300

    5.7. Ипотека [#156315 #258021]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        while (amount >= 0) {
+        while (amount > 0) {
            amount = ((amount * percent / 100) + amount) - salary;
             year++;
         }
