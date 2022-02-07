98db5f05b1a2c3e66f648ed20e7bfee59ba6c0e3
irina glazunova <zvepushka0@gmail.com>
2022-02-07 09:02:56 +0300

    6.5. Поиск индекса в диапазоне. [#156318 #261262]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -14,7 +14,7 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenFind3() {
+    public void whenArrayHas2Then3() {
         int[] data = new int[] {5, 2, 10, 2, 4};
         int el = 2;
         int start = 2;
