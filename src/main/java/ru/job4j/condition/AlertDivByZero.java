f790548cdad3584b41b91911c5dd461ed8b31a2e
irina glazunova <zvepushka0@gmail.com>
2022-02-23 21:53:31 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -10,7 +10,6 @@ public class AlertDivByZero {
         if (number == 0) {
             System.out.println("Could not div by 0.");
         }
-
         if (number < 0) {
             System.out.println("This is negative number.");
         }
